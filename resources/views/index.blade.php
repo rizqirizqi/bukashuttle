b0a5f9c3e53f7ed2f2404e22205e6c3c1506e366
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2017-09-30 23:28:19 +0700

    Clean up

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="theme-color" content="#009688" />
+  <title>BukaShuttle</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+  <header>
+    <nav>
+      <div class="nav-wrapper teal">
+        <a href="#" class="brand-logo center">BukaShuttle</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+	<main id="app">
+    <div class="container u-mrgn-top--2">
+      <div class="row">
+        <div class="col s12">
+          
+        </div>
+      </div>
+    </div>
+  </main>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+  <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
