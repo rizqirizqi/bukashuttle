cfa632915e2c27d78c9c74fe4e28f34aba4d9441
Ryan Baskara <ryan.baskara@bukalapak.com>
2017-10-01 08:47:34 +0700

    done summary page

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -5,12 +5,44 @@
 
 @section('body')
   <div class="container u-mrgn-top--6">
+  	<h4 style="color:#d71149"><b>Orders</b></h2>
     <div class="row">
-      <div class="col s12 center">
-        <div class="">
-          
+      <div class="col s12">
+        <div class="row" style="margin-bottom: 0!important">
+      	  	<div class="col s6">
+          		<p style="font-size: 18px"><b>Route</b></p>
+          	</div>
+          	<div class="col s6">
+          		<p style="font-size: 18px">PCV - Jason's</p>
+          	</div>
         </div>
+        <div class="row">
+      	  	<div class="col s6">
+          		<p style="font-size: 18px"><b>Departure Time</b></p>
+          	</div>
+          	<div class="col s6">
+          		<p style="font-size: 18px">17.30</p>
+          	</div>
+        </div>
+        <div class="row">
+        	<div class="col s12 center">
+        		<a class="waves-effect btn" style="background-color: #d71149">Batalkan</a>
+        	</div>
+        </div>
+        <hr/>
       </div>
+    </div>
+    <h4 style="color:#d71149"><b>Passengers</b></h2>
+    <div class="row">
+    	<div class="col s12">
+    		<ul class="collection" style="border-top: 0!important; border-right: 0!important; border-left: 0!important; box-shadow: none!important;">
+		      <li class="collection-item">Fadhilur Rizki ( @fadhilurrizki )</li>
+		      <li class="collection-item">Alvin Yaputra ( @alvinyap )</li>
+		      <li class="collection-item">Mgs. M. Rizqi Fadhlurrahman ( @mgsrizqi )</li>
+		      <li class="collection-item">Ryan Baskara Putra ( @ryanbaskara )</li>
+		      <li class="collection-item">Anita Carolina Aritonang (@anitaritonang )</li>
+		    </ul>
+    	</div>
     </div>
   </div>
 @endsection
